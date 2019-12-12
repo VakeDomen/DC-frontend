109191cac9508f40373e1249d31ce876dae87eec
Vake Domen <vake.domen94@gmail.com>
2019-12-12 15:43:58 +0100

    auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +34,7 @@ import { PrivateDashboardComponent } from './pages/private-dashboard/private-das
 import { GroupsComponent } from './pages/groups/groups.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NewNoteComponent } from './components/new-note/new-note.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 
 @NgModule({
@@ -76,7 +77,12 @@ import { NewNoteComponent } from './components/new-note/new-note.component';
   exports: [
     MatDatepickerModule,
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: AuthInterceptor, multi: true 
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(
+        private authenticationService: AuthService,
+        private router: Router,    
+    ) { }
+    
+    
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 401) {
+                // auto logout if 401 response returned from api
+                this.authenticationService.logout();
+                this.router.navigate(["/login"]);
+            }
+
+            const error = err.error.message || err.statusText;
+            return throwError(error);
+        }))
+    }
+}
